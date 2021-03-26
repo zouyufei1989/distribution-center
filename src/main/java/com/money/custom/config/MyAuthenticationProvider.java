fb7d20a01585f537a43f662596694d09e51696fe
upgrade <zou.yufei@pandabus.cn>
2021-03-26 09:41:53 +0800

    开发环境，验证码随意登录

--- src/main/java/com/money/custom/config/MyAuthenticationProvider.java
+++ src/main/java/com/money/custom/config/MyAuthenticationProvider.java
@@ -2,6 +2,7 @@ package com.money.custom.config;
 
 import com.money.custom.entity.enums.ConstsEnum;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -15,14 +16,19 @@ import java.util.Objects;
 
 public class MyAuthenticationProvider extends DaoAuthenticationProvider {
 
+    @Value("${spring.profiles.active}")
+    String ENV;
+
     @Override
     protected void additionalAuthenticationChecks(UserDetails userDetails, UsernamePasswordAuthenticationToken authentication) throws AuthenticationException {
         HttpServletRequest req = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-        String code = req.getParameter("verifyCode");
-        String verify_code = (String) req.getSession().getAttribute(ConstsEnum.LOGIN_VERIFY_CODE.getName());
-        if (Objects.isNull(code) || Objects.isNull(verify_code) || !StringUtils.equals(code.toLowerCase(),verify_code.toLowerCase())) {
-            logger.error(String.format("sys:%s user:%s", verify_code, code));
-            throw new AuthenticationServiceException("验证码错误");
+        if (!StringUtils.equals(ENV, "dev")) {
+            String code = req.getParameter("verifyCode");
+            String verify_code = (String) req.getSession().getAttribute(ConstsEnum.LOGIN_VERIFY_CODE.getName());
+            if (Objects.isNull(code) || Objects.isNull(verify_code) || !StringUtils.equals(code.toLowerCase(), verify_code.toLowerCase())) {
+                logger.error(String.format("sys:%s user:%s", verify_code, code));
+                throw new AuthenticationServiceException("验证码错误");
+            }
         }
         super.additionalAuthenticationChecks(userDetails, authentication);
     }
