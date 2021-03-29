535db4bcdaa156adcfabebcba1b51509feeca4c1
upgrade <zou.yufei@pandabus.cn>
2021-03-29 21:21:22 +0800

    fix 订单消耗记录，json格式改为单独查询包含的子消耗内容

--- src/main/java/com/money/custom/dao/OrderConsumptionDao.java
+++ src/main/java/com/money/custom/dao/OrderConsumptionDao.java
@@ -1,6 +1,11 @@
 package com.money.custom.dao;
 
+import com.money.custom.entity.OrderItemConsumption;
 import com.money.framework.base.dao.BaseDao;
 
+import java.util.List;
+
 public interface OrderConsumptionDao extends BaseDao {
+
+    List<OrderItemConsumption> selectConsumptionItemsByConsumptionId(Integer orderId);
 }
--- src/main/java/com/money/custom/dao/impl/OrderConsumptionDaoImp.java
+++ src/main/java/com/money/custom/dao/impl/OrderConsumptionDaoImp.java
@@ -1,12 +1,19 @@
 package com.money.custom.dao.impl;
 
 import com.money.custom.dao.OrderConsumptionDao;
+import com.money.custom.entity.OrderItemConsumption;
 import com.money.framework.base.annotation.SQLContext;
 import com.money.framework.base.dao.impl.BaseDaoImpl;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 @SQLContext(nameSpace = "OrderConsumption")
 public class OrderConsumptionDaoImp extends BaseDaoImpl implements OrderConsumptionDao {
 
+    @Override
+    public List<OrderItemConsumption> selectConsumptionItemsByConsumptionId(Integer orderId) {
+        return selectList("selectConsumptionItemsByConsumptionId", orderId);
+    }
 }
